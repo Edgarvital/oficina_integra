33735028adecfc8b4e0075a62f2d417b3d076beb
GuilhermeGz <guilherme.dna06@gmail.com>
2022-06-25 19:07:27 -0300

    Adicionada relações as entidades e atributos para atribuição em massa

--- app/Tarefa.php
+++ app/Tarefa.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tarefa extends Model
 {
-    //
+
+    protected $fillable = [
+        'nome', 'flag_concluida', 'atividade_id'
+    ];
+
+    public function atividade(){
+        return $this->belongsTo('App\Atividade', 'atividade_id');
+    }
+
 }
